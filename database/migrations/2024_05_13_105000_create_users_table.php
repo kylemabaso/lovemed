985e4bccabe7f1e59d90750a8c21be1e27999278
Kyle Mabaso <kylemabaso@gmail.com>
2024-05-17 13:09:39 +0200

    Update 2024_05_13_105000_create_users_table.php

--- database/migrations/2024_05_13_105000_create_users_table.php
+++ database/migrations/2024_05_13_105000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->text('id_number')->unique();
+            $table->unique('id_number(255)', 'id_number_index');
             $table->string('date_of_birth');
             $table->foreignId('gender_id')->nullable()->constrained();
             $table->foreignId('language_id')->constrained();
