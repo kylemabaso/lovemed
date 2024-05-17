043adcc80891eb490fba70ffcd604be0dd8d735a
Kyle Mabaso <kylemabaso@gmail.com>
2024-05-17 12:44:27 +0200

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,7 +15,6 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        Role::create(['name' => 'Admin']);
         Role::create(['name' => 'Doctor']);
         Role::create(['name' => 'Patient']);
         Role::create(['name' => 'Pharmacist']);
