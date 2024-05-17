b7e5a1526b32a7959e2e7302d7db25dc3e66e064
Kyle Mabaso <kylemabaso@gmail.com>
2024-05-17 12:55:10 +0200

    Restored permission seeder.
    I assumed there was conflict between the permission seeder and the impersonate package only to realise I was running migrate instead of migrate:refresh.

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,6 +15,7 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
+        Role::create(['name' => 'Admin']);
         Role::create(['name' => 'Doctor']);
         Role::create(['name' => 'Patient']);
         Role::create(['name' => 'Pharmacist']);
